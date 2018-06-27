39fcbec59941ad24798a20c0b1865a1dca1d46a0
tostilord <louis.voet@gmail.com>
2018-06-27 16:45:20 +0000

    update

--- scripts/get_ids.py
+++ scripts/get_ids.py
@@ -1,9 +1,6 @@
 import sys
 
-#file = open(sys.argv[0], "r")
-#file = open("test.txt", "r")
-file = open("../data/RNA-Seq-counts.txt", "r")
-
+file = open(sys.argv[1], "r")
 ids = ""
 for line in file.readlines():
     if line[:2] == "lp":
@@ -11,10 +8,3 @@ for line in file.readlines():
 file.close()
 
 print(ids[:-1])
-#sys.argv[1] = ids[:-1]
-
-#result = open(sys.argv[1], "w")
-result = open("../data/ids.txt", "w")
-result.write(ids[:-1])
-result.close()
-
